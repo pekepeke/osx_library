1a1b00dbf73d910d3955c47c0f2569d0aaa3114d
pekepeke <pekepekesamurai@gmail.com>
2012-10-04 00:38:14 +0900

    update

--- set_preference.sh
+++ set_preference.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 opt_apply=0
+APPLICATIONS="KeyRemap4MacBook GeekTool Spectacle"
 usage() {
   prg_name=`basename $0`
   cat <<EOM
@@ -13,6 +14,18 @@ preferences() {
   find "Preferences" -type f
 }
 
+ctl_applications() {
+  local cmd="killall"
+  local opt=""
+  if [ x$1 != xstop ]; then
+    cmd=open
+    opt="-a"
+  fi
+  for app in $APPLICATIONS; do
+    ${cmd} ${opt} ${app}
+  done
+}
+
 yes_or_no() {
   local ret
   if [ "x$1" != x ]; then
@@ -23,6 +36,7 @@ yes_or_no() {
   [ "x$ret" = "xy" ]
   return $?
 }
+
 trace() {
   echo $*
   $*
@@ -31,12 +45,13 @@ trace() {
 main() {
   cd $(dirname $0)
   local tm=$(date +'%S')
+  ctl_applications stop
   for f in $(preferences); do
     local TARGET="$HOME/Library/$f"
     if [ x$opt_apply = 1 ]; then
       if yes_or_no "copy $f -> $TARGET" ; then
         cp -irp $TARGET /tmp/$(basename $TARGET).library
-        [ ! -e $TARGET.org ] && mv $TARGET $TARGET.org
+        [ ! -e $TARGET.org -a ! -L $TARGET ] && mv $TARGET $TARGET.org
         trace cp $f $TARGET
       fi
     else
@@ -46,6 +61,7 @@ main() {
       fi
     fi
   done
+  ctl_applications start
 }
 
 while getopts "hvs:" opt; do
