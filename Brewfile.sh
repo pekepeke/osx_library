458a8572065fd55b83d95fa0eefc93826b7d2440
pekepeke <pekepekesamurai+github@gmail.com>
2015-03-20 20:08:13 +0900

    split config file

--- Brewfile.sh
+++ Brewfile.sh
@@ -54,6 +54,13 @@ installed_cask() {
   return 1
 }
 
+reload_gui() {
+  killall Dock
+
+  killall Finder
+  sudo killall -KILL appleeventsd
+}
+
 # install homebrew {{{1
 if ! is_executable xcode-select; then
   echo "please install Xcode.app" 1>&2
@@ -65,6 +72,61 @@ if ! is_executable brew; then
 fi
 brew_prefix=$(brew --prefix)
 [ -w $brew_prefix ] && sudo chown -R $USER $brew_prefix
+
+# defaults {{{1
+
+# memo bool = yes
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool yes
+# memo bool = no
+# defaults delete com.apple.finder _FXShowPosixPathInTitle
+
+ # アプリケーションの「保存」ダイアログで、詳細な保存場所を指定できる「詳細表示」をデフォルトに設定
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Tabで全てのコントロールにフォーカスできるようになる
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# 拡張子を常に表示する
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# 隠しファイル表示
+# defaults write com.apple.finder AppleShowAllFiles -bool YES
+
+# ネットワーク経由時、.DS_Store の作成を抑制
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# サービスのコンテキストメニューで階層を作らないように
+defaults write -g NSServicesMinimumItemCountForContextSubmenu -int 15
+
+#Finderのパスバーの開始位置をホームフォルダに変更
+#defaults write com.apple.finder PathBarRootAtHome -bool yes
+
+# 現在の位置をフルパス表示
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool yes
+# defaults delete com.apple.finder _FXShowPosixPathInTitle
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# QuickLook のテキスト選択を有効
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# safari ウィングルウィンドウモード
+defaults write com.apple.Safari TargetedClicksCreateTabs -bool true
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Dashboard 無効
+defaults write com.apple.dashboard mcx-disabled -boolean true
+
+# X11 click
+defaults write org.x.x11 wm_click_through -bool true
+
+# dock の背景を半透明に
+defaults write com.apple.dock hide-mirror -bool true
+
+# reload_gui
 
 # taps {{{1
 brew tap homebrew/binary || true
@@ -196,6 +258,7 @@ brew install gibo || true
 brew install mercurial || true
 brew install subversion --unicode-path || true
 brew install tig
+brew install icdiff
 
 brew install qt4
 brew install sip
@@ -205,183 +268,12 @@ brew install pyqt
 
 brew install sqlite
 brew install mysql || true
+brew install percona-toolkit
 brew install sqlite || true
 brew install mongodb || true
 brew install elasticsearch || true
 brew install boot2docker
 
 brew install vim --with-lua --with-python --with-ruby || true
-
-# cask packages {{{1
-# browser {{{2
-brew cask install firefox
-# brew cask install chromium
-brew cask install google-chrome
-
-# runtime {{{2
-brew cask install silverlight
-brew cask install java
-brew cask install flash
-brew cask install flip4mac
-brew cask install adobe-air
-brew cask install easysimbl
-
-# file {{{2
-brew cask install dropbox
-brew cask install google-drive
-
-# communication {{{2
-# cask install join-me
-brew cask install skype
-
-# utilities {{{2
-# cask install keyremap4macbook
-brew cask install clamxav
-brew cask install karabiner
-brew cask install xtrafinder
-brew cask install bettertouchtool
-# brew cask install alfred
-brew cask install flashlight
-brew cask install flashlighttool
-brew cask install google-japanese-ime
-brew cask install menumeters
-brew cask install slate
-brew cask install accessmenubarapps
-brew cask install dash
-brew cask install geektool
-
-brew cask install takemepop
-brew cask install clipmenu
-brew cask install appcleaner
-
-brew cask install zipeg
-brew cask install keka
-brew cask install hardcore-zipper
-brew cask install macwinzipper
-brew cask install the-unarchiver
-
-brew cask install caffeine
-brew cask install timemachineeditor
-brew cask install day-o
-brew cask install grandperspective
-brew cask install xquartz
-brew cask install dupscanub
-brew cask install disk-inventory-x
-brew cask install magican
-brew cask install permanent-eraser
-brew cask install tnef-s-enough
-brew cask install key-codes
-brew cask install growl-fork
-
-## Multimedia {{{2
-brew cask install vlc
-brew cask install tomahawk
-# cask install google-music
-brew cask install radiant-player
-# brew cask install google-music-manager
-brew cask install music-manager
-brew cask install handbrake
-brew cask install skitch
-brew cask install xnviewmp
-brew cask install gravit
-brew cask install atraci
-# TODO : https://dribbble.com/shots/1582290-PSD-to-PNG-Automator-Action
-
-# own packages {{{2
-brew cask install macvim-kaoriya-custom
-brew cask install zeal
-brew cask install kdiff3-ja
-
-## Editor {{{2
-brew cask install coteditor
-brew cask install textmate
-brew cask install intellij-idea-ce
-brew cask install unity3d
-brew cask install textwrangler
-# cask install nethackcocoa
-# macvim-kaoriya
-# cask install sublime-text
-# emacs
-
-brew cask install 0xed
-
-brew cask install nvalt
-brew cask install livediag
-# cask install lightpaper
-# cask install haroopad
-brew cask install libreoffice
-brew cask install freemind
-
-## Dev Tools {{{2
-brew cask install iterm2
-brew cask install diffmerge
-brew cask install p4merge
-brew cask install stroy
-brew cask install virtualbox
-brew cask install vagrant
-
-brew cask install cyberduck
-brew cask install charles
-brew cask install sequel-pro
-brew cask install pg-commander
-brew cask install astah-community
-brew cask install dbeaver-community
-brew cask install robomongo
-brew cask install pixus
-# brew cask install sim-pholders2
-brew cask install simulator-manager
-brew cask install iexplorer
-brew cask install speedlimit
-
-# cask install reveal
-brew cask install google-web-designer
-brew cask install rapid-svn
-brew cask install sourcetree
-brew cask install img2icns
-brew cask install integrity
-brew cask install sitesucker
-
-brew cask install platypus
-brew cask install fluid
-brew cask install genymotion
-
-# gifzo
-brew cask install phoneclean
-# brew cask install keepassx
-brew cask install macpass
-
-# quicklook {{{2
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install quicklook-json
-brew cask install qlprettypatch
-brew cask install quicklook-csv
-brew cask install betterzipql
-brew cask install webp-quicklook
-brew cask install suspicious-package
-brew cask install qlimagesize
-# cask install qlmarkdown
-brew cask install multimarkdown-qlgenerator
-brew cask install fjn-web-archive-ql
-brew cask install thong-ql
-brew cask install ql-sqlite3
-brew cask install provisionql
-
-# colorpicker {{{2
-brew cask install colorpicker-skalacolor
-brew cask install colorpicker-propicker
-
-# Memo {{{1
-cat <<EOM
-brew cask install cooviewer
-brew cask install mplayer-osx-extended
-brew cask install timemachineeditor
-brew cask install dupscanub
-EOM
-
-# TODO
-# v2c
-# toyviewer
-# pix compare
 
 # __END__ {{{1
